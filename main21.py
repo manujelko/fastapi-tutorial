acbd1913056dbba110aaf3752d4bef7a1adcd45e
Manuel Šarić <manujelko@gmail.com>
2023-01-02 12:58:03 +0100

    Deprecating parameters.

---
+++ main21.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(
+        q: str | None = Query(
+            default=None,
+            alias="item-query",
+            title="Query string",
+            description="Query string for...",
+            min_length=3,
+            max_length=50,
+            regex="^fixedquery$",
+            deprecated=True,
+        )
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
