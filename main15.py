76ad955aa6245a982ed971350f01efe065d751a2
Manuel Šarić <manujelko@gmail.com>
2023-01-02 09:42:45 +0100

    Add regular expession.

---
+++ main15.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(
+        q: str | None = Query(
+            default=None,
+            min_length=3,
+            max_length=50,
+            regex="^fixedquery$"
+        )
+):
+    result = {
+        "items": [
+            {"item_id": "Foo"},
+            {"item_id": "Bar"},
+        ]
+    }
+    if q:
+        result.update({"q": q})
+    return result
